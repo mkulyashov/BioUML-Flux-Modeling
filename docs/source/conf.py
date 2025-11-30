1247b15bb1fb9dfcde787a3de289d02f8930ba99
mkulyashov <45638539+mkulyashov@users.noreply.github.com>
2025-12-01 00:44:37 +0300

    fixpath

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 project = "BioUML Flux Modeling"
-author = "Your Name"
+author = "mkulyashov, vkachnov, mesembaeva, emelikhova"
 release = "0.1.0"
 
 extensions = [
